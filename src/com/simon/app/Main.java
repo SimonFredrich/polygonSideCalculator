15276bb82feb4cc598e43c3f6ccbd43d5c5f855d
SimonFredrich <21simplex02@gmail.com>
2019-03-08 10:23:37 +0100

    creation: calculator class

--- src/com/simon/app/Main.java
+++ src/com/simon/app/Main.java
@@ -3,6 +3,12 @@ package com.simon.app;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        int x, y;
+        float pi = (float) Math.PI;
+
+        System.out.println(pi);
+
+        
+
     }
 }
---
+++ src/com/simon/app/calculator/Calculator.java
@@ -0,0 +1,7 @@
+package com.simon.app.calculator;
+
+public class Calculator {
+    public Calculator() {
+
+    }
+}
