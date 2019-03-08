379ef705bafb962febe361a88fb512bddd813557
SimonFredrich <21simplex02@gmail.com>
2019-03-08 16:45:17 +0100

    better code structure

--- src/com/simon/app/Main.java
+++ src/com/simon/app/Main.java
@@ -1,11 +1,17 @@
 package com.simon.app;
 
+import com.simon.app.calculator.Calculator;
 import com.simon.app.user.Input;
 
 public class Main {
 
     public static void main(String[] args) {
-        new Input();
+        Input input = new Input();
+        double r = input.radius();
+        int side = input.side();
+
+        Calculator calculator = new Calculator();
+        System.out.println(calculator.computeOneSideLengthOfPolygon(r, side));
     }
 
 }
--- src/com/simon/app/user/Input.java
+++ src/com/simon/app/user/Input.java
@@ -8,20 +8,12 @@ public class Input {
 
     private Scanner s = new Scanner(System.in);
 
-    public Input() {
-        int side = side();
-        double r = radius();
-
-        Calculator calculator = new Calculator();
-        System.out.println(calculator.computeOneSideLengthOfPolygon(r, side));
-    }
-
-    private double radius() {
+    public double radius() {
         System.out.println("Give me the radius: ");
         return s.nextDouble();
     }
 
-    private int side() {
+    public int side() {
         System.out.println("Give me the sides: ");
         return s.nextInt();
     }
