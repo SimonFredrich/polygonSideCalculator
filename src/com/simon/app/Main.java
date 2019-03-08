30cb48ffabe2bdb06b1ff45138c89f483303c44e
SimonFredrich <21simplex02@gmail.com>
2019-03-08 11:07:44 +0100

    sentence of the pythagoras

--- src/com/simon/app/Main.java
+++ src/com/simon/app/Main.java
@@ -3,12 +3,19 @@ package com.simon.app;
 public class Main {
 
     public static void main(String[] args) {
-        int x, y;
+        int x = 0, y = 0;
         float pi = (float) Math.PI;
 
-        System.out.println(pi);
-
         
+
+        int point1X = 3, point1Y = 2;
+        int point2X = 2, point2Y = 4;
+
+        int xAchse = point1X - point2X;
+        int yAchse = point2Y - point1Y;
+
+        var result = Math.sqrt((xAchse*xAchse) + (yAchse*yAchse));
+        System.out.println(result);
 
     }
 }
